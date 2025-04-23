5f51ea58a4fb8756f1d37947db3d832e04377926
minik <minikusy19@gmail.com>
2025-04-23 17:52:46 +0200

    Stworzenie menu opcji z możliwością: wyświetlania; dodawania; usuwania; zapisywania oraz wychodzenia z aplikacji. Aplikacja działa na podstawie operowania danych w json'ie.

---
+++ main.py
@@ -0,0 +1,60 @@
+import json
+#
+# Odczytywanie
+# Opening JSON file
+#with open('Slownik.json', 'r') as openfile:
+#	# Reading from json file
+#	slownik = json.load(openfile)
+#
+#print(slownik)
+#
+#noweimie = input("Jakie imię chcesz dodać? : ")
+#nowaocena = input("Jaką ocenę otrzymała ta osoba? : ")
+#
+#slownik[noweimie] = nowaocena
+#
+#print(slownik)
+#
+#ocenadousuniecia = input("Jakiego ucznia (z oceną) chcesz usunąć? : ")
+#
+#slownik.pop(ocenadousuniecia)
+#
+# Tworzenie słownika
+#Json_object = json.dumps(slownik, indent = 4)
+#
+# Owieranie pliku jako outfile
+#with open("Slownik.json","w") as outfile:
+#	outfile.write(Json_object)
+#--------------------------------------
+#wpisz:
+#1 - wyswietl
+#2 - dodaj
+#3 - usun
+#4 - zapisz i wyjdz
+
+with open('Slownik.json', 'r') as openfile:
+    # Reading from json file
+    slownik = json.load(openfile)
+
+while True:
+    opcja = input('''Wybierz opcję: 
+    1 - wyswietl
+    2 - dodaj
+    3 - usun
+    4 - zapisz i wyjdz''')
+    if opcja == '1':
+        print(slownik)
+    elif opcja == '2':
+        noweimie = input("Jakie imię chcesz dodać? : ")
+        nowaocena = input("Jaką ocenę otrzymała ta osoba? : ")
+        slownik[noweimie] = nowaocena
+    elif opcja == '3':
+        ocenadousuniecia = input("Jakiego ucznia (z oceną) chcesz usunąć? : ")
+        slownik.pop(ocenadousuniecia)
+    elif opcja == '4':
+        # Tworzenie słownika
+        Json_object = json.dumps(slownik, indent=4)
+        # Owieranie pliku jako outfile
+        with open("Slownik.json", "w") as outfile:
+            outfile.write(Json_object)
+        break
--- plik.py
+++ plik.py
@@ -1,1 +1,21 @@
-abc
+import json
+slownik = { 'Ola':'5', 'Pola':'4', 'Tola':'3'}
+
+n = input("Jakie imię chcesz dodać? : ")
+m = input("Jaką ocenę otrzymała ta osoba? : ")
+
+slownik[n] = m
+
+print(slownik)
+
+Json_object = json.dumps(slownik, indent = 4)
+
+with open("Slownik.json","w") as outfile:
+	outfile.write(Json_object)
+
+# Opening JSON file
+with open('Slownik.json', 'r') as openfile:
+	# Reading from json file
+	czytanyslownik = json.load(openfile)
+
+print(czytanyslownik)
